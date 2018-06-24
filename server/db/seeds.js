2576d25c0b57269d3bffa38a3b18f16f751bd456
patty-cc <iain.paterson.cc@gmail.com>
2018-06-24 17:11:19 +0100

    Add more models to lambo data

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -10,6 +10,72 @@ db.lambo.insert([
     speed:"158mph"
   },
   {
+    model:"400 GT",
+    dates:"1966-1968",
+    engine:"V12 3.929 litre",
+    speed:"155mph"
+  },
+  {
+    model:"Miura",
+    dates:"1966-1972",
+    engine:"V12 3.929 litre",
+    speed:"180mph"
+  },
+  {
+    model:"Espada",
+    dates:"1968-1978",
+    engine:"V12 3.929 litre",
+    speed:"152mph"
+  },
+  {
+    model:"Islero",
+    dates:"1968-1969",
+    engine:"V12 3.929 litre",
+    speed:"154mph"
+  },
+  {
+    model:"Jarma",
+    dates:"1970-1976",
+    engine:"V12 3.929 litre",
+    speed:"150mph"
+  },
+  {
+    model:"Urraco",
+    dates:"1973-1979",
+    engine:"V8 2,0 litre",
+    speed:"140mph"
+  },
+  {
+    model:"Countach",
+    dates:"1974-1990",
+    engine:"V12 4.0 litre",
+    speed:"158mph"
+  },
+  {
+    model:"Silhouette",
+    dates:"1976-1979",
+    engine:"V8 3.0 litre",
+    speed:"160mph"
+  },
+  {
+    model:"Jalpa",
+    dates:"1981-1988",
+    engine:"V8 3.5 litre",
+    speed:"145mph"
+  },
+  {
+    model:"LM002",
+    dates:"1986-1993",
+    engine:"V12 5.17 litre",
+    speed:"130mph"
+  },
+  {
+    model:
+    dates:
+    engine:
+    speed:
+  },
+  {
     model:
     dates:
     engine:
