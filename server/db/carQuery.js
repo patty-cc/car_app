5aee3dcd4f1f21d159a999b3ae869eab59ed1ac4
patty-cc <iain.paterson.cc@gmail.com>
2018-06-24 18:26:41 +0100

    Add further changes to CarsQuery file

--- server/db/carQuery.js
+++ server/db/carQuery.js
@@ -1,7 +1,13 @@
 var MongoClient = require('mongodb').MongoClient;
 
-Var carQuery = function(){
+Var carQuery = function(data){
+  MongoClient.connect("mongodb://localhost:27017/cars", function(err, db) {
+    db.collection("lambo").find().toArray(function(err, docs) {
+      console.log(docs);
 
+      data(docs);
+    })
+  })
 };
 
 module.exports = CarQuery;
