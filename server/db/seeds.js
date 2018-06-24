62ecdd0120db6296fcfd5ccf5828cef042e6e163
patty-cc <iain.paterson.cc@gmail.com>
2018-06-24 16:59:34 +0100

    Add file structure and seeds for database

---
+++ server/db/seeds.js
@@ -0,0 +1,36 @@
+use cars;
+
+db.dropdatabase();
+
+db.lambo.insert([
+  {
+    model:"350 GT",
+    dates:"1964-1966",
+    engine:"V12 3.464 litre",
+    speed:"158mph"
+  },
+  {
+    model:
+    dates:
+    engine:
+    speed:
+  },
+  {
+    model:
+    dates:
+    engine:
+    speed:
+  },
+  {
+    model:
+    dates:
+    engine:
+    speed:
+  },
+  {
+    model:
+    dates:
+    engine:
+    speed:
+  },
+]);
